20f1fa16c98ebb400c12570082ee953c27792e06
aniecres <111096788+aniecres@users.noreply.github.com>
2022-10-27 22:40:29 -0400

    update files

--- Assets/script.js
+++ Assets/script.js
@@ -5,8 +5,18 @@ let backBtn = document.getElementById('goBackBtn');
 let clearHighScoreBtn = document.getElementById('clearHighscoreBtn');
 let time = document.getElementById('timerTag'); 
 
+ 
+
 
 highScoresBtn.addEventListener('click', myFunction);
  function myFunction() {
     
  }
+
+ const questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {"<script>", "<js>", "<scirpt>", "<javascript>"}
+        correctAns: "<script>"
+    }
+ ]
